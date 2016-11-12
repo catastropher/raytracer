7cd657bb3224b2c64ceca810f12a0f35bb8d1099
Michael Wilder <mwilder.w@gmail.com>
2016-11-12 11:56:16 -0500

    Added a check for when running the program with arguments

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 	cout << "Hello world!" << endl;
+
+	if(argc > 1) {
+		cout << "This program doesn't take any arguments!" << endl;
+	}
 }
